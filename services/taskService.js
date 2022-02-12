3cfe712a3c99fdd787f26e9e0ab0189f75e502a7
Leonardo <leo_mc_inter@hotmail.com>
2022-02-12 17:44:40 -0300

    Criado o arquivo de validação da tarefa

--- services/taskService.js
+++ services/taskService.js
@@ -1,6 +1,12 @@
 const { createTaskModel } = require('../models/taskModel');
+const { badRequest } = require('../utils/dictionary');
+const { taskSchema } = require('../utils/schemas');
 
 const createTaskService = async (task, status) => {
+  
+  const { error } = taskSchema.validate({ task, status });
+  if (error) return res.status(badRequest).json({ message: error.message });
+
   const id = await createTaskModel(task, status);
 
   return {
