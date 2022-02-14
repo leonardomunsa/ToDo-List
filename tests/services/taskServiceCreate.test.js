39afa70c249f6f75e00f7691a5daf9fe5453c048
Leonardo <leo_mc_inter@hotmail.com>
2022-02-13 23:35:42 -0300

    Criados testes para criação de tarefa nos services

--- tests/services/taskServiceCreate.test.js
+++ tests/services/taskServiceCreate.test.js
@@ -6,13 +6,13 @@ const TaskService = require('../../services/taskService');
 
 describe('Inserts a new task', () => {
   describe('when the payload is not valid', () => {
-    const payloadTask = {
+    const payloadTaskWrong = {
       task: 15,
     };
 
     it('sould throw an error object', async () => {
       try {
-        await TaskService.createTaskService(payloadTask);
+        await TaskService.createTaskService(payloadTaskWrong);
       } catch (error) {
         expect(error).to.be.a('object');
       }
@@ -20,7 +20,7 @@ describe('Inserts a new task', () => {
 
     it('the error object should contain the property status equal to 400', async () => {
       try {
-        await TaskService.createTaskService(payloadTask);
+        await TaskService.createTaskService(payloadTaskWrong);
       } catch (error) {
         expect(error).to.be.have.property('status').equal(400);
       }
@@ -28,11 +28,29 @@ describe('Inserts a new task', () => {
 
     it('the error object should contain the message expected', async () => {
       try {
-        await TaskService.createTaskService(payloadTask);
+        await TaskService.createTaskService(payloadTaskWrong);
       } catch (error) {
         expect(error.message).to.be.equal('Must have task and status and both must be strings');
       }
     });
+  });
 
+  describe('when the payload is valid', () => {
+    const payloadTask = {
+      task: 'Eat meal',
+      status: 'pending',
+    };
+
+    it('sould return a object', async () => {
+      const response = await TaskService.createTaskService(payloadTask.task, payloadTask.status);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('the object should contain the task "Eat meal"', async () => {
+      const response = await TaskService.createTaskService(payloadTask.task, payloadTask.status);
+
+      expect(response).to.have.property('task').to.eq('Eat meal');
+    });
   });
 });
