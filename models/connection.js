1627861468ab23ec13dd84f6e4160325f24a0b8d
Leonardo <leo_mc_inter@hotmail.com>
2022-02-17 00:14:47 -0300

    Protegido o url de conexão com mongodb atlas

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb+srv://Munsa:${process.env.MONGO_DB_PASSWORD}@cluster0.hz2el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
 const DB_NAME = 'todolist';
 
 module.exports = () => mongodb.connect(MONGO_DB_URL, {
